e809ff459f0c701e9fc4d49984e3267220d95f44
Shaurya Awasthi <shaurya.awasthi130@gmail.com>
2026-01-22 00:36:06 +0530

    fixed broken packages

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,7 +9,6 @@ import close from "./close.svg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
@@ -18,7 +17,6 @@ import nextjs from "./tech/nextjs.png";
 import postgres from "./tech/postgres.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
@@ -47,7 +45,6 @@ export {
   close,
   css,
   docker,
-  figma,
   git,
   html,
   javascript,
@@ -55,7 +52,6 @@ export {
   nextjs,
   nodejs,
   reactjs,
-  redux,
   tailwind,
   typescript,
   threejs,
